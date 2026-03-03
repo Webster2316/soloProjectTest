82b264a8d7c99e8de918dd6abc3face655064cae
Webster2316 <Alaina.24@ichat.sp.edu.sg>
2026-03-04 00:31:45 +0800

    updated tables and made routes files

---
+++ src/routes/publicChatRoute.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+//const publicChatController = require("../controllers/publicChatController");
+
+
+// POST /api/parents/register
+// router.post("/register", parentController.registerParent);
+
+
+module.exports = router;
---
+++ src/routes/userRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+//const userController = require("../controllers/userController");
+
+
+// POST - create new user upon new session detection
+// router.post("/", userController.createUser);
+
+//get user by public chat id
+
+//put - update user info (e.g. username)
+
+//delete - delete account by user
+
+
+
+
+module.exports = router;
