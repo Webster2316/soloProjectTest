5ac17460f05520b513fb8c08e7524dcd5d8e424e
Webster2316 <Alaina.24@ichat.sp.edu.sg>
2026-03-13 01:30:17 +0800

    basic chatroom baackend so send msgs

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const path = require("path");
 const { authMiddleware } = require('./middleware/auth.middleware.js');
 const userRoutes = require('./routes/userRoutes.js');
 const authRoutes = require('./routes/authRoutes.js');
+const chatroomRoutes = require('./routes/chatroomRoutes');
 
 const app = express();
 app.use(express.json());
@@ -21,5 +22,12 @@ app.use(authMiddleware);
 
 app.use('/api', authRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/chatroom', chatroomRoutes);
+
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Internal Server Error' });
+});
 
 module.exports = app;
